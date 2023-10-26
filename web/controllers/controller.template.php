b856355b01d625cdc24f82b749dc96b82e338c67
Neos Lab <neo@neoslab.dev>
2023-10-26 10:38:15 -0600

    [add] adminlte assets

--- web/controllers/controller.template.php
+++ web/controllers/controller.template.php
@@ -2,9 +2,29 @@
 
 class TemplateController {
 
+    /**
+     * Principal view of template
+     *
+     * @return html
+     */
     public function index() {
 
         include "views/template.php";
+
+    }
+
+    /**
+     * Domain of site
+     *
+     * @return string
+     */
+    public static function path() {
+
+        if ( !empty( $_SERVER['HTTPS'] ) && ( "on" == $_SERVER['HTTPS'] ) ) 
+            return "https://" . $_SERVER['SERVER_NAME'] . "/ecommerce/web/";
+
+        else
+            return "http://" . $_SERVER['SERVER_NAME'] . "/ecommerce/web/";
 
     }
 
